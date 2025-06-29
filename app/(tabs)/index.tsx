017775c09ec52fa3f485ab772402860ec027a2eb
ahonaroy05 <ahonaroy2005@gmail.com>
2025-06-29 23:19:45 +0530

    Fix duplicate ScrollView closing tag

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -166,7 +166,6 @@ export default function HomeScreen() {
             </View>
           </View>
         </View>
-      </ScrollView>
 
       <FloatingActionButton 
         onPress={() => setIsBreathing(!isBreathing)}
